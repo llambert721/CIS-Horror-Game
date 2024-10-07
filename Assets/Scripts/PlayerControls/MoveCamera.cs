941280349318ac4c53da5be85370f6207dce59e7
Logan Lambert <96006583+llambert721@users.noreply.github.com>
2024-10-06 23:32:13 -0500

    Test
    setup scene

--- Assets/Scripts/PlayerControls/MoveCamera.cs
+++ Assets/Scripts/PlayerControls/MoveCamera.cs
@@ -6,6 +6,11 @@ public class MoveCamera : MonoBehaviour
 {
     public Transform cameraPostion;
 
+    void Start()
+    {
+        transform.position = cameraPostion.position;
+    }
+
     // Update is called once per frame
     void Update()
     {
