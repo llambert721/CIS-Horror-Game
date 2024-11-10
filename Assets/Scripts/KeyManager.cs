4a7b3f42ebc24fe171d8bb7c92ef70b2834c0762
Logan Lambert <96006583+llambert721@users.noreply.github.com>
2024-11-09 20:24:26 -0600

    Added cargo
    Add "cargo" locations for key spawns

--- Assets/Scripts/KeyManager.cs
+++ Assets/Scripts/KeyManager.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class KeyManager : MonoBehaviour
 {
+    public GameObject key;
+    public List<Transform> spawnPoints;
     public bool hasKey;
+
     // Start is called before the first frame update
     void Start()
     {
         hasKey = false;
+
+        Transform spawnPoint = PickSpawnPoint();
+        Spawn(spawnPoint, key);
     }
 
     public void UpdateKey()
     {
         hasKey = true;
+    }
+
+    public Transform PickSpawnPoint()
+    {
+        int randomIndex = Random.Range(0, spawnPoints.Count);
+
+        return spawnPoints[randomIndex];
+    }
+
+    void Spawn(Transform spawnPoint, GameObject spawnedObject)
+    {
+        spawnedObject.transform.position = spawnPoint.position;
+
+        RaycastHit hit;
+        if (Physics.Raycast(spawnPoint.position, Vector3.down, out hit))
+        {
+            // Move the object to sit flush with the table based on the hit point
+            Vector3 newPosition = hit.point;
+            newPosition.y += spawnedObject.GetComponent<Collider>().bounds.extents.y;
+            spawnedObject.transform.position = newPosition;
+        }
+        else
+        {
+            Debug.LogWarning("No table detected below the spawn point!");
+        }
     }
 }
